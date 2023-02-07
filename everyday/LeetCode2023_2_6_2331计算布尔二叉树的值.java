7c49bcd27afa131a392fa97277c44764141c2d48
Xavier <xavierzy@163.com>
2023-02-07 19:57:34 +0800

    2023.2.7

---
+++ everyday/LeetCode2023_2_6_2331计算布尔二叉树的值.java
@@ -0,0 +1,16 @@
+package everyday;
+
+import publicClass.TreeNode;
+
+public class LeetCode2023_2_6_2331计算布尔二叉树的值 {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
