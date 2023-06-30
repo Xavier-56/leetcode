c804308d6b9a8ba17a1a3852e6dfefaf6de8905d
Xavier <xavierzy@163.com>
2023-06-30 15:00:36 +0800

    2023.6.30

---
+++ everyday/LeetCode2023_6_30_2490回环句.java
@@ -0,0 +1,15 @@
+package everyday;
+
+public class LeetCode2023_6_30_2490回环句 {
+    public boolean isCircularSentence(String sentence) {
+        if (sentence.charAt(sentence.length() - 1) != sentence.charAt(0)) {
+            return false;
+        }
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
