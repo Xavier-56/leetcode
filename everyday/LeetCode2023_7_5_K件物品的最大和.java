60f40d0014a3dfee606bb87e0450ddfc9b359161
Xavier <xavierzy@163.com>
2023-07-05 11:18:32 +0800

    2023.7.5

---
+++ everyday/LeetCode2023_7_5_K件物品的最大和.java
@@ -0,0 +1,10 @@
+package everyday;
+
+public class LeetCode2023_7_5_K件物品的最大和 {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if (k <= numOnes) return k;
+        else if (k <= numZeros + numOnes) {
+            return numOnes;
+        } else return numOnes - (k - numOnes - numZeros);
+    }
+}
